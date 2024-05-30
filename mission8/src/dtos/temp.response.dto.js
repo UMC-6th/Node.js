c21c982862aa6b38baaa89d97d69d118f54a5ac3
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-05-30 19:11:20 +0900

    feat: 8주차 미션

---
+++ mission8/src/dtos/temp.response.dto.js
@@ -0,0 +1,17 @@
+export const tempResponseDTO = (data) => {
+    return {"testString" : data};
+}
+export const flagResponseDTO = (flag) => {
+    return {"flag" : flag};
+}
+
+export const userResponseDTO = (rows) => {
+    const result = [];
+    
+    for (let i = 0; i < rows.length; i++) {
+        const element = rows[i];
+        result.push({"username": element.name, "userage": element.age});
+    }
+
+    return result;
+}
