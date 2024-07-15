f9278fb37f2bc2b22f6958c24f80115f7688a861
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-07-15 23:57:55 +0900

    chore: 내가 작성한 리뷰 목록 API 구현

--- mission10_1/src/providers/user.provider.js
+++
@@ -1,7 +0,0 @@
-import { previewReviewResponseDTO } from "../dtos/user.dto.js"
-import { getPreviewReview } from "../models/user.dao.js"
-
-export const getReview = async (storeId, query) => {
-    const {reviewId, size = 3} = query;
-    return previewReviewResponseDTO(await getPreviewReview(reviewId, size, storeId));
-}
--- mission10_1/src/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import asyncHandler from 'express-async-handler';
-import { userSignin, userMission, reviewPreview } from "../controllers/user.controller.js";
-
-export const userRouter = express.Router({mergeParams: true});
-
-userRouter.post('/signin', asyncHandler(userSignin));
-userRouter.post('/mission', asyncHandler(userMission));
-storeRouter.get('/reviews', asyncHandler(reviewPreview));
