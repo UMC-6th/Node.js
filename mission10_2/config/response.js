6481cd25f688ca8598b7ca488c34d4edd96e4a7b
Seoyeon <154313093+seoyeoneel02@users.noreply.github.com>
2024-07-17 00:40:12 +0900

    chore: Delete mission10_2 directory

--- mission10_2/config/response.js
+++
@@ -1,9 +0,0 @@
-export const response = ({isSuccess, code, message, status}, result) => {
-    return {
-        isSuccess: isSuccess,
-        code: code,
-        message: message,
-        status: status,
-        result: result
-    }
-};
